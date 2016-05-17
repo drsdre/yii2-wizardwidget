3ebe930aea038f6637afff9589a1c85fbfeb9a06
Lawiet <ljorgelgonzalez@gmail.com>
2016-05-17 09:20:57 -0400

    Files with fix

--- js/wizardwidget.js
+++ js/wizardwidget.js
@@ -4,7 +4,6 @@ $(document).ready(function () {
 
     //Wizard
     $('a[data-toggle="tab"]').on('show.bs.tab', function (e) {
-
         var $target = $(e.target);
 
         if ($target.parent().hasClass('disabled')) {
@@ -13,23 +12,51 @@ $(document).ready(function () {
     });
 
     $(".next-step").click(function (e) {
-
         var $active = $('.wizard .nav-tabs li.active');
-        $active.next().removeClass('disabled');
-        nextTab($active);
+        var $next = $active.next();
+        var $function = jQuery(this).attr('data-function') || false;
 
+        if($function){
+            var callback = function(){
+                $next.removeClass('disabled');
+                nextTab($active);
+            };
+
+            eval($function);
+        }else{
+            $next.removeClass('disabled');
+            nextTab($active);
+        }
     });
+
     $(".prev-step").click(function (e) {
-
         var $active = $('.wizard .nav-tabs li.active');
-        prevTab($active);
+        var $function = jQuery(this).attr('data-function') || false;
 
+        if($function){
+            var callback = function(){
+                prevTab($active);
+            };
+
+            eval($function);
+        }else{
+            prevTab($active);
+        }
+    });
+
+    $(".save-step").click(function (e) {
+        var $function = jQuery(this).attr('function') || false;
+
+        if($function){
+            eval($function);
+        }
     });
 });
 
 function nextTab(elem) {
     $(elem).next().find('a[data-toggle="tab"]').click();
 }
+
 function prevTab(elem) {
     $(elem).prev().find('a[data-toggle="tab"]').click();
 }
